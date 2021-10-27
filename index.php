5659cdbffd01b6fd74c20a65cdf66e0527296d3e
Cheboksarov Yaroslav <yacheboksarov768@gmail.com>
2021-10-27 18:34:19 +0300

    Исправлены ошибки форматирования

--- index.php
+++ index.php
@@ -4,88 +4,90 @@ use JetBrains\PhpStorm\Pure;
 
 session_start();
 //запоминаем время начала работы скрипта
-  $start = microtime(true);
+$start = microtime(true);
 //получаем дату и время по москве
-  date_default_timezone_set('Europe/Moscow');
-  $now= date("d.m.y H:i");
+date_default_timezone_set('Europe/Moscow');
+$now = date("d.m.y H:i");
 //получаем параметры из main.html
-  $x = $_GET["x"];
-  $y = $_GET["y"];
-  $r = $_GET["R"];
-  //Hit check functions
-  global $message;
-  #[Pure] function checkHit($x, $y, $r): bool
-  {
-    return checkTriangle($x,$y,$r) ||
-    checkSq($x,$y,$r) || checkSector($x,$y,$r);
-  }
-  #[Pure] function checkTriangle($x, $y, $r): bool
-  {
-    $S1 = squareArea(0,0,$x,$y,$r/2,0);
-    $S2 = squareArea(0,$r/2,$x,$y,$r/2,0);
-    $S3 = squareArea(0,0,$x,$y,$r/2,0);
-    $S = squareArea(0,0,0,$r/2,$r/2,0);
+$x = $_GET["x"];
+$y = $_GET["y"];
+$r = $_GET["R"];
+//Hit check functions
+global $message;
+#[Pure] function checkHit($x, $y, $r): bool
+{
+    return checkTriangle($x, $y, $r) ||
+        checkSq($x, $y, $r) || checkSector($x, $y, $r);
+}
+
+#[Pure] function checkTriangle($x, $y, $r): bool
+{
+    $S1 = squareArea(0, 0, $x, $y, $r / 2, 0);
+    $S2 = squareArea(0, $r / 2, $x, $y, $r / 2, 0);
+    $S3 = squareArea(0, 0, $x, $y, $r / 2, 0);
+    $S = squareArea(0, 0, 0, $r / 2, $r / 2, 0);
     $Sum = $S1 + $S2 + $S3;
-      return !($Sum > $S);
+    return !($Sum > $S);
 
-  }
-  #[Pure] function squareArea($x1, $y1, $x2, $y2, $x3, $y3): float
-  {
-    $a = module($x1,$y1,$x2,$y2);
-    $b = module($x2,$y2,$x3,$y3);
-    $c = module($x3,$y3,$x1,$y1);
-    $p = $a+$b+$c;
-    return sqrt($p*($p-$a)*($p-$b)*($p-$c));
-  }
-  function module($x1,$y1, $x2,$y2): float
-  {
-    return sqrt(($x2-$x1)^2 + ($y2-$y1)^2);
-  }
+}
 
-  function checkSq($x,$y,$r): bool
-  {
+#[Pure] function squareArea($x1, $y1, $x2, $y2, $x3, $y3): float
+{
+    $a = module($x1, $y1, $x2, $y2);
+    $b = module($x2, $y2, $x3, $y3);
+    $c = module($x3, $y3, $x1, $y1);
+    $p = $a + $b + $c;
+    return sqrt($p * ($p - $a) * ($p - $b) * ($p - $c));
+}
+
+function module($x1, $y1, $x2, $y2): float
+{
+    return sqrt(($x2 - $x1) ^ 2 + ($y2 - $y1) ^ 2);
+}
+
+function checkSq($x, $y, $r): bool
+{
     return $x >= 0 && $x <= $r &&
-    $y <= 0 && $y >= -$r;
-  }
-  function checkSector($x,$y,$r): bool
-  {
-    return $x <= 0 && $y >=0 &&
-    sqrt($x^2 + $y^2) <= $r;
-  }
-  #[Pure] function hitInfo($x, $y, $r): string
-  {
-    if(checkHit($x,$y,$r)){
-      $message = "Точка попала";
-    }else
-	{
-	  $message = "Точка не попала";
-	}
-	return $message;
-  }
-  function sessionNumber()
-  {
-	  if (!isset($_SESSION['count']))
-{
-   $_SESSION['count']= 1;
-}
-else
-{
-  ++$_SESSION['count'];
+        $y <= 0 && $y >= -$r;
 }
 
-  }
+function checkSector($x, $y, $r): bool
+{
+    return $x <= 0 && $y >= 0 &&
+        sqrt($x ^ 2 + $y ^ 2) <= $r;
+}
 
-  //получаем время окончания работы скрипта
-  $finish = microtime(true);
-	//высчитываем время работы (разницу) и округляем
-  $timeWork=$finish-$start;
-  $timeWork=round($timeWork,7);
-  //заполняем переменную сессии для отображения всей таблицы
-  sessionNumber();
-  $message = hitInfo($x,$y,$r);
-  $sessionNumber = $_SESSION['count'];
-  $_SESSION['tableRw'.$sessionNumber] =
-  "<tr>
+#[Pure] function hitInfo($x, $y, $r): string
+{
+    if (checkHit($x, $y, $r)) {
+        $message = "Точка попала";
+    } else {
+        $message = "Точка не попала";
+    }
+    return $message;
+}
+
+function sessionNumber()
+{
+    if (!isset($_SESSION['count'])) {
+        $_SESSION['count'] = 1;
+    } else {
+        ++$_SESSION['count'];
+    }
+
+}
+
+//получаем время окончания работы скрипта
+$finish = microtime(true);
+//высчитываем время работы (разницу) и округляем
+$timeWork = $finish - $start;
+$timeWork = round($timeWork, 7);
+//заполняем переменную сессии для отображения всей таблицы
+sessionNumber();
+$message = hitInfo($x, $y, $r);
+$sessionNumber = $_SESSION['count'];
+$_SESSION['tableRw' . $sessionNumber] =
+    "<tr>
 	  <td>$x</td>
 	  <td>$y</td>
 	  <td>$r</td>
@@ -103,7 +105,7 @@ echo "<table>
     	  <td>Current time</td>
       </tr>
     </table>";
-    foreach ($_SESSION as $value) {
-      echo $value;
-    }
+foreach ($_SESSION as $value) {
+    echo $value;
+}
 
--- validator.js
+++ validator.js
@@ -2,39 +2,41 @@
 
 let x, y, r;
 
-function defineValues(){
+function defineValues() {
     x = document.querySelector('input[name="x"]:checked').value;
     y = document.querySelector('input[id="Y"]').value;
     r = document.querySelector('select[id="R"]').value;
 }
+
 document.getElementById("SendData").onclick = function () {
     defineValues();
     if (validateX() && validateY()) {
-      fetch(createRequest()).then(response => response.text()).then(serverAnswer => {
-          document.getElementById("out").innerHTML = serverAnswer;
-      }).catch(err => createNotification("Ошибка HTTP. Повторите попытку позже. " + err));
-  }
+        fetch(createRequest()).then(response => response.text()).then(serverAnswer => {
+            document.getElementById("out").innerHTML = serverAnswer;
+        }).catch(err => createNotification("Ошибка HTTP. Повторите попытку позже. " + err));
+    }
 };
-function createRequest(){
+
+function createRequest() {
     let path = 'index.php?x='
-    + x + '&y='
-    + y + '&R='
-    + r;
+        + x + '&y='
+        + y + '&R='
+        + r;
     let header = new Headers();
     header.append('Content-Type', 'application/x-www-form-url')
-    let init = {method:'GET', headers:header};
+    let init = {method: 'GET', headers: header};
     return new Request(path, init);
 }
+
 function createNotification(message) {
     alert("Something went wrong " + message);
 }
 
 function validateX() {
-    if(typeof x === 'undefined'){
+    if (typeof x === 'undefined') {
         createNotification("x не выбран");
         return false;
-    }
-    else {
+    } else {
         return true;
     }
 }
